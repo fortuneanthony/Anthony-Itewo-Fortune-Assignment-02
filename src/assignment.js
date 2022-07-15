af169cba72909861430e2f7ab480871550b8b578
Fortune Anthony-Itewo <109281466+fortuneanthony@users.noreply.github.com>
2022-07-15 22:06:14 +0000

    new attempt

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   let sum = arrayOfNumbers.reduce(function(prev, curr) {     return prev + curr;
+})
+  
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
+
+
 
 /**
  * Challenge - 2
@@ -27,10 +32,19 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
+
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let noOfEvenNumbers=0
+    for(i=0; i<arrayOfNumbers.length; i++){
+    if(arrayOfNumbers[i]%2===0){
+      noOfEvenNumbers++
+    }
+  }
+    return noOfEvenNumbers
+     return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
+
 
 /**
  * Challenge - 3
